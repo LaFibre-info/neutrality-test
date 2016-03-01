09b2c407e8966b92b6bae67d14e65d44932f6374
kgersen <kgersen@hotmail.com>
2016-03-01 23:21:23 +0100

    - doc & & readme

--- README.md
+++ README.md
@@ -1,12 +1,29 @@
 # neutrality-test
 test your ISP neutrality
 
-WIP WIP WIP
+    neutrality-test [options]
+     Options:
+       -debug           display debug informations
+       -help            brief help message
+       -4               IPv4 only
+       -6               IPv6 only
+       -csv             output results as a 'database ready' table
+       -test "<test>"   performs the given test
+       -size <size>     change size
+       -ul              perform only upload tests
+       -dl              perform only download tests
+       -time <value>    timeout each test after <value> seconds
+       -server <server> specify server (dns name or IP)
 
-TODO 
-
-- more error checks
-- delete tmp file when ctrl-c
-- separate up/dl size
-- dl test data from an url ?
-- handle nspeed url format vs testdebit url format ?
+    <test> format = "IP PORT PROTO EXT DIR"
+      IP = 4 or 6
+      PORT = a valid TCP port
+      PROT = http or https
+      EXT  = any file extention with a leading dot (ex: .zip)
+      DIR  = GET or POST
+    <size> format = <value> or <value>/<value>
+      <value> = <number> or <number>[KMGT]
+      a single <value> set both upload & download size to the same value
+      a double <value>/<value> set download (1st) and upload (2nd) distinct sizes
+      K, M, G,T denote: Kilo, Mega, Giga and Tera
+      for instance "2G/20M" set 2GB download size and 20MB upload size
--- neutrality-test.pl
+++ neutrality-test.pl
@@ -16,7 +16,47 @@ use POSIX ":sys_wait_h";
 use IO::Handle;
 use Config;
 
-our $VERSION = 1.0.2;
+our $VERSION = 1.0.3;
+
+=pod
+
+=head1 NAME
+  My::Module
+
+=head1 SYNOPSIS
+  neutrality-test
+  test your ISP neutrality
+
+=head1 DESCRIPTION
+neutrality-test [options]
+ Test your ISP neutrality
+ Options:
+   -debug           display debug informations
+   -help            brief help message
+   -4               IPv4 only
+   -6               IPv6 only
+   -csv             output results as a 'database ready' table
+   -test "<test>"   performs the given test
+   -size <size>     change size
+   -ul              perform only upload tests
+   -dl              perform only download tests
+   -time <value>    timeout each test after <value> seconds
+   -server <server> specify server (dns name or IP)
+
+<test> format = "IP PORT PROTO EXT DIR"
+  IP = 4 or 6
+  PORT = a valid TCP port
+  PROT = http or https
+  EXT  = any file extention with a leading dot (ex: .zip)
+  DIR  = GET or POST
+<size> format = <value> or <value>/<value>
+  <value> = <number> or <number>[KMGT]
+  a single <value> set both upload & download size to the same value
+  a double <value>/<value> set download (1st) and upload (2nd) distinct sizes
+  K, M, G,T denote: Kilo, Mega, Giga and Tera
+  for instance "2G/20M" set 2GB download size and 20MB upload size
+
+=cut
 
 # parameters & constants
 my $debug = 0;
@@ -42,7 +82,7 @@ GetOptions(
   '-4' => \$ip4only,
   '-6' => \$ip6only,
   'debug' => \$debug,
-  'help|?' => sub { pod2usage(1) }) or pod2usage(2);
+  'help' => sub { pod2usage(-verbose => 1) }) or pod2usage( {-verbose => 2 });
 
 # parse -size <value>
 sub ParseSize {
@@ -338,23 +378,3 @@ __DATA__
 6 6881 http  .zip POST
 6 8080 http  .zip POST
 end
-
-=TODO
-=pod
-=head1 neutrality-test
-test your ISP neutrality
-=head1 SYNOPSIS
-neutrality-test [options]
- Options:
-   -debug           display debug informations
-   -help            brief help message
-=head1 OPTIONS
-=over 8
-=item B<-help>
-Print a brief help message and exits.
-=item B<-debug>
-Prints display debug informations.
-=back
-=head1 DESCRIPTION
-B<This program> TODO.
-=cut
